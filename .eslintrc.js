2cdcd685bfe66366f5bd56a82c234255c80111cc
davidholyko <david.holy.ko@gmail.com>
2022-04-30 12:11:07 -0400

    fix(app): add linting, update nvmrc

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'import'],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          //
+          'builtin',
+          'internal',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
